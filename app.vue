8fea0af9f3fb8954f2d112962c53d345008eb1a1
Daniel Roe <daniel@roe.dev>
2022-05-27 13:56:12 +0200

    fix: add styling and head metadata

--- app.vue
+++ app.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="flex flex-col gap-4 px-4 items-center justify-center">
+    <Html lang="en-GB" />
+    <Title>n3dium</Title>
+    <Meta
+      name="description"
+      content="A tiny, incomplete example for a paywalled content site built in Nuxt 3 for a live-coding session at Vue.js Global Summit 2022."
+    />
     <header class="w-full max-w-[960px]">
       <nav>
         <ul>
-          <li><NuxtLink to="/">n3dium</NuxtLink></li>
+          <li>
+            <NuxtLink to="/" class="flex items-center gap-2 text-green-400">
+              <img
+                alt="Nuxt logo"
+                src="https://nuxtjs.org/design-kit/colored-logo.svg"
+                height="24"
+                width="24"
+              />
+              <strong>n3dium</strong>
+            </NuxtLink>
+          </li>
         </ul>
         <ul>
           <li>
@@ -18,9 +34,52 @@
     <main class="w-full max-w-[960px]">
       <NuxtPage />
     </main>
+    <footer>
+      <small>
+        Made with ❤️ &middot;
+        <a href="https://github.com/danielroe/n3dium">Contributions welcome</a>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script setup>
 import '@picocss/pico'
 </script>
+
+<style>
+/* Blue-grey Light scheme (Default) */
+/* Can be forced with data-theme="light" */
+[data-theme='light'],
+:root:not([data-theme='dark']) {
+  --primary: #546e7a;
+  --primary-hover: #455a64;
+  --primary-focus: rgba(84, 110, 122, 0.125);
+  --primary-inverse: #fff;
+}
+/* Blue-grey Dark scheme (Auto) */
+/* Automatically enabled if user has Dark mode enabled */
+@media only screen and (prefers-color-scheme: dark) {
+  :root:not([data-theme='light']) {
+    --primary: #546e7a;
+    --primary-hover: #607d8b;
+    --primary-focus: rgba(84, 110, 122, 0.25);
+    --primary-inverse: #fff;
+  }
+}
+/* Blue-grey Dark scheme (Forced) */
+/* Enabled if forced with data-theme="dark" */
+[data-theme='dark'] {
+  --primary: #546e7a;
+  --primary-hover: #607d8b;
+  --primary-focus: rgba(84, 110, 122, 0.25);
+  --primary-inverse: #fff;
+}
+/* Blue-grey (Common styles) */
+:root {
+  --form-element-active-border-color: var(--primary);
+  --form-element-focus-color: var(--primary-focus);
+  --switch-color: var(--primary-inverse);
+  --switch-checked-background-color: var(--primary);
+}
+</style>
